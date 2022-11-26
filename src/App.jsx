4fb3f054347051c2999f3cd5bc0a1d7686b41449
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-11-26 18:28:45 +0500

    Front end Client side Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Risgister";
+import Login from "./pages/login/Login";
 const App = () => {
   return (
   <>
+  {/* <Login/> */}
   <Register/>
   {/* <Watch/> */}
   {/* <Home/> */}
